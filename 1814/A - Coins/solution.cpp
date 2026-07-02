0ce95af03eb8e19f0c0fde826e0146c1c7d52b7e
AYUSH CHANDRA <ayush13chandra@gmail.com>
2026-07-02 21:05:38 +0530

    Add Coins [A] from Codeforces

---
+++ 1814/A - Coins/solution.cpp
@@ -0,0 +1,28 @@
+ #include <bits/stdc++.h>
+        using namespace std;
+       
+     int main() {
+            ios::sync_with_stdio(false);
+            cin.tie(nullptr);
+       
+            int t;
+            cin >> t;
+           while (t--) {
+               long long n, k;
+               cin >> n >> k;
+      
+               if (n % 2 == 0) {
+                   cout << "YES
+";
+               } else {
+                   if (n >= k && (n - k) % 2 == 0) {
+                    cout << "YES
+";
+                   } else {
+                       cout << "NO
+";
+                   }
+             }
+           }
+           return 0;
+       }
