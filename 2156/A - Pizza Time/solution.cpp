2be775f2077384bfecae9c7384e38a9456cee036
AYUSH CHANDRA <ayush13chandra@gmail.com>
2026-07-02 20:56:42 +0530

    Add Pizza Time [A] from Codeforces

---
+++ 2156/A - Pizza Time/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+     long long t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        if (n==3) cout<<1<<'
+';
+        else cout<<1+(n-3)/2<<'
+';
+       
+    }
+       
+ return 0;
+ 
+ 
+}
