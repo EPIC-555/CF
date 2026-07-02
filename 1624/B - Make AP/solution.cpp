a0488b048e8c7b489fa1ff218a057e53d20c4f83
AYUSH CHANDRA <ayush13chandra@gmail.com>
2026-07-02 21:11:17 +0530

    Add Make AP [B] from Codeforces

---
+++ 1624/B - Make AP/solution.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+ 
+{
+ 
+    int t;
+ 
+    cin >> t;
+ 
+    while (t--)
+ 
+    {
+ 
+        int a, b, c;
+ 
+        cin >> a >> b >> c;
+ 
+ 
+ 
+        if ((2 * b - c) % a == 0 && (2 * b - c) / a > 0)
+ 
+        {
+ 
+            cout << "YES";
+ 
+        }
+ 
+        else if ((a + c) % (2 * b) == 0 && (a + c) / (2 * b) > 0)
+ 
+        {
+ 
+            cout << "YES";
+ 
+        }
+ 
+        else if ((2 * b - a) % c == 0 && (2 * b - a) / c > 0)
+ 
+        {
+ 
+            cout << "YES";
+ 
+        }
+ 
+        else
+ 
+            cout << "NO";
+ 
+        cout << endl;
+ 
+    }
+ 
+ 
+ 
+    return 0;
+ 
+}
